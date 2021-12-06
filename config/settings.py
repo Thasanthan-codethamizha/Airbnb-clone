36486389ab0cbe1d703882e066c0a68e9a01fd51
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-12-06 10:35:18 +0530

    apps initialization

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'reviews',
+    'lists',
+    'reservations',
+    'conversations',
 ]
 
 MIDDLEWARE = [
